62f3593b2ebc33ad330259d1adc53d1c7e4de338
ReFil <31960031+ReFil@users.noreply.github.com>
2023-11-22 14:43:56 +0000

    feat(split): Add peripheral is bonded/connected to central
    For easy use by displays and indicators it would be helpful to be able to see if any peripheral at an index is connected and bonded

--- app/include/zmk/split/bluetooth/central.h
+++ app/include/zmk/split/bluetooth/central.h
@@ -6,3 +6,6 @@
 
 int zmk_split_bt_invoke_behavior(uint8_t source, struct zmk_behavior_binding *binding,
                                  struct zmk_behavior_binding_event event, bool state);
+
+bool zmk_split_bt_central_peripheral_is_connected(uint8_t index);
+bool zmk_split_bt_central_peripheral_is_bonded(uint8_t index);
