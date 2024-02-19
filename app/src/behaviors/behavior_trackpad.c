373c9578908981c7b071f84fd3216a1040414593
ReFil <31960031+ReFil@users.noreply.github.com>
2024-02-19 08:38:22 +0000

    Behaviour improvements

--- app/src/behaviors/behavior_trackpad.c
+++ app/src/behaviors/behavior_trackpad.c
@@ -10,10 +10,11 @@
 #include <drivers/behavior.h>
 #include <zephyr/logging/log.h>
 
+#include <zmk/behavior.h>
+
 #include <dt-bindings/zmk/trackpad.h>
 #include <zmk/trackpad.h>
 #include <zmk/hid.h>
-#include <zmk/keymap.h>
 
 LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
@@ -21,8 +22,9 @@ LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
 static int behavior_trackpad_init(const struct device *dev) { return 0; }
 
-static int on_keymap_binding_pressed(struct zmk_behavior_binding *binding,
-                                     struct zmk_behavior_binding_event event) {
+static int on_trackpad_keymap_binding_pressed(struct zmk_behavior_binding *binding,
+                                              struct zmk_behavior_binding_event event) {
+    LOG_DBG("tp behaviuor hit");
     switch (binding->param1) {
     case TP_TOG: {
         zmk_trackpad_set_enabled(!zmk_trackpad_get_enabled());
@@ -52,17 +54,17 @@ static int on_keymap_binding_pressed(struct zmk_behavior_binding *binding,
     return -ENOTSUP;
 }
 
-static int on_keymap_binding_released(struct zmk_behavior_binding *binding,
-                                      struct zmk_behavior_binding_event event) {
+static int on_trackpad_keymap_binding_released(struct zmk_behavior_binding *binding,
+                                               struct zmk_behavior_binding_event event) {
     return ZMK_BEHAVIOR_OPAQUE;
 }
 
 static const struct behavior_driver_api behavior_trackpad_driver_api = {
-    .binding_pressed = on_keymap_binding_pressed,
-    .binding_released = on_keymap_binding_released,
+    .binding_pressed = on_trackpad_keymap_binding_pressed,
+    .binding_released = on_trackpad_keymap_binding_released,
 };
 
-DEVICE_DT_INST_DEFINE(0, behavior_trackpad_init, NULL, NULL, NULL, APPLICATION,
-                      CONFIG_KERNEL_INIT_PRIORITY_DEFAULT, &behavior_trackpad_driver_api);
+BEHAVIOR_DT_INST_DEFINE(0, behavior_trackpad_init, NULL, NULL, NULL, APPLICATION,
+                        CONFIG_KERNEL_INIT_PRIORITY_DEFAULT, &behavior_trackpad_driver_api);
 
 #endif /* DT_HAS_COMPAT_STATUS_OKAY(DT_DRV_COMPAT) */
